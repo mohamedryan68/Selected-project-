c2755f21b02bcb9b25ab1656b4b12e3f98b6edde
mohamedryan68 <mohamed.ryan68@yahoo.com>
2016-12-09 15:17:06 +0200

    Update selected_project_rules.pl

--- selected_project_rules.pl
+++ selected_project_rules.pl
@@ -1,17 +1,17 @@
 rule(1,
-     lhs( [av(charging_lamp_off, yes),av(electricity_depend, yes) ] ),
-     rhs( av(problem, charger), 90) ).
+     lhs( [not(av(computer_turn_on,yes)),av(charging_lamp_off, yes),av(electricity_depend, yes) ] ),                                                                                   
+     rhs( av(problem, charger), 90) ).                                         
 
 rule(2,
-     lhs( [av(no_battery_exist, yes),av(power_in_laptop, no)] ),
+     lhs( [av(no_battery_exist, yes),not(av(power_in_laptop, yes))] ),
      rhs( av(electricity_depend, yes), 80) ).
 
 rule(3,
-    lhs( [av(not_charged,yes)]),
+    lhs( [not(av(computer_turn_on,yes)),av(not_charged,yes)]),
     rhs( av(problem, battery),90) ).
 
 rule(4,
-     lhs( [not(av(not_charged,yes)),not(av(power_in_laptop,yes))] ),
+     lhs( [not(av(computer_turn_on,yes)),not(av(not_charged,yes)),not(av(power_in_laptop,yes))] ),
      rhs( av(problem,battery),90) ).
 
 
@@ -20,45 +20,45 @@ rule(5,
      rhs( av(not_charged,yes),90) ).
 
 rule(6,
-     lhs( [av(labtop_fall_down,yes)] ),
+     lhs( [not(av(desktop_start,yes)),av(computer_turn_on,yes),av(labtop_fall_down,yes)] ),
      rhs( av(problem,ram),80) ).
 
 rule(7,
-     lhs( [av(bios_screen,yes)] ),
+     lhs( [not(av(desktop_start,yes)),av(computer_turn_on,yes),av(bios_screen,yes)] ),
      rhs( av(problem,ram),100) ).
 
 rule(8,
-     lhs( [av(labtop_fall_down, yes),av(bad_resoluation, yes) ] ),
+     lhs( [av(desktop_start,yes),av(computer_turn_on,yes),av(labtop_fall_down, yes),av(bad_resoluation, yes) ] ),
      rhs( av(problem, displaycard), 80) ).
 
 rule(9,
-     lhs( [av(labtop_fall_down, yes),av(red_lines, yes) ] ),
+     lhs( [av(desktop_start,yes),av(computer_turn_on,yes),av(labtop_fall_down, yes),av(red_lines, yes) ] ),
      rhs( av(problem, displayflat), 90) ).
 
 rule(10,
-     lhs( [av(hear_os_start, yes),av(black_display, yes) ] ),
+     lhs( [av(computer_turn_on,yes),av(hear_os_start, yes),av(black_display, yes) ] ),
      rhs( av(problem, displaymode), 90) ).
 
 rule(11,
-     lhs( [av(hear_os_start, yes),av(labtop_fall_down, yes) ] ),
+     lhs( [av(computer_turn_on,yes),av(hear_os_start, yes),av(labtop_fall_down, yes) ] ),
      rhs( av(problem, display), 90) ).
 
 
 rule(12,
-     lhs( [not(av(hear_fan_sound,yes))] ) ,
+     lhs( [not(av(desktop_start,yes)),av(computer_turn_on,yes),not(av(hear_fan_sound,yes))] ) ,
      rhs( av(problem, cpufan), 90)).
 
 
 rule(13,
-     lhs( [av(uncom_sound, yes),av(aircondition, yes) ] ),
+     lhs( [not(av(desktop_start,yes)),av(computer_turn_on,yes),av(uncom_sound, yes),av(aircondition, yes) ] ),
      rhs( av(problem, cpufan), 90) ).
 
 rule(14,
-     lhs( [av(high_temp,yes)]),
+     lhs( [not(av(desktop_start,yes)),av(computer_turn_on,yes),av(high_temp,yes)]),
      rhs( av(aircondition,yes), 100) ).
 
 rule(15,
-     lhs( [av(windows_do_con_op, yes),av(flash_screen, yes) ] ),
+     lhs( [not(av(desktop_start,yes)),av(computer_turn_on,yes),av(windows_do_con_op, yes),av(flash_screen, yes) ] ),
      rhs( av(problem, os), 100) ).
 
 rule(16,
@@ -66,19 +66,19 @@ rule(16,
      rhs( av(windows_do_con_op,yes), 100)).
 
 rule(17,
-     lhs( [av(windows_op_disk_restart, yes) ] ),
+     lhs( [av(desktop_start,yes),av(computer_turn_on,yes),av(windows_op_disk_restart, yes) ] ),
      rhs( av(problem, os), 100) ).
 
 rule(18,
-     lhs( [av(windows_in_deadlock, yes) ] ),
+     lhs( [av(desktop_start,yes),av(computer_turn_on,yes),av(windows_in_deadlock, yes) ] ),
      rhs( av(problem, os), 100) ).
 
 rule(19,
-     lhs( [av(blue_page, yes) ] ),
+     lhs( [not(av(desktop_start,yes)),av(computer_turn_on,yes),av(blue_page, yes) ] ),
      rhs( av(problem, os), 100) ).
 
 rule(20,
-     lhs( [av(can_not_load_os, yes) ] ),
+     lhs( [not(av(desktop_start,yes)),av(computer_turn_on,yes),av(can_not_load_os, yes) ] ),
      rhs( av(problem, harddrive), 90) ).
 
 
@@ -87,11 +87,11 @@ rule(21,
      rhs(av(can_not_load_os,yes), 100)).
 
 rule(22,
-     lhs( [av(smell_smoke, yes) ] ),
+     lhs( [not(av(computer_turn_on,yes)),av(smell_smoke, yes) ] ),
      rhs( av(problem, board), 90) ).
 
  rule(23,
-     lhs( [av(fall_drink, yes) ] ),
+     lhs( [not(av(computer_turn_on,yes)),av(fall_drink, yes) ] ),
      rhs( av(problem, board), 90) ).   
 
   rule(24,
@@ -99,18 +99,24 @@ rule(22,
      rhs( av(problem, board), 90) ).
 
  rule(25,
-     lhs( [av(power_lamp_off, yes) ] ),
+     lhs( [not(av(charger,yes)),not(av(computer_turn_on,yes)) ] ),
      rhs( av(problem, powerbutton), 90) ).
 
 
+askable(computer_turn_on, 'Does the computer turn on  : ').
+askable(desktop_start, 'Does the desktop start : ').
 askable(charging_lamp_off, 'Does the charging lamp off : ').
-askable(no_battery_exist , 'there exist battery our not : ').
+askable(no_battery_exist , 'no battery exist : ').
 askable(power_in_laptop, 'Do you feel power in labtop : ').
+askable(charger_connected,' Does the charger connected :').
 askable(labtop_fall_down, 'Does the labtop fall down : ').
+askable(red_lines, 'Do you see red lines  : ').
 askable(bios_screen, 'Does Bios screen doesnot count ram : ').
 askable(bad_resoluation, 'Does the screen get bad resoluation : ').
+askable(flash_screen, 'Does the screen make flashes : ').
 askable(hear_os_start, 'Do you here os start : ').
 askable(black_display, 'Do you see black display : ').
+askable(high_temp, 'Do you feel high temperature: ').
 askable(hear_fan_sound, 'Do you here fan sound : ').
 askable(uncom_sound, 'Do you here uncomfortable sound :  ').
 askable(windows_restart_when_load, 'Does os restart when load its files : ').
@@ -121,4 +127,6 @@ askable(comp_restart, 'Does computer restart : ').
 askable(fall_drink, 'Does drink droped on computer  : ').
 askable(smell_smoke, 'Do you smell smoke : ').
 askable(power_lamp_off, 'Does power lamp is off : ').
+askable(blue_page, 'Do you see bule page : ').
+
 
